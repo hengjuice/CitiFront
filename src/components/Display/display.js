4f784c6ecd7f7ab2acaaa8e4654d456fec8e9c56
malcolmpang19 <70058856+malcolmpang19@users.noreply.github.com>
2022-08-11 21:38:50 +0800

    display.js

--- src/components/Display/display.js
+++ src/components/Display/display.js
@@ -1,16 +1,61 @@
-import React from 'react'
 import Avatar from '@mui/material/Avatar';
 import PersonIcon from '@mui/icons-material/Person';
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import {Button, Paper} from '@mui/material';
+
 const Display = (props) => {
   const clientInfo = props.clientInfo
+  //fetch client info 
+  const [isLoading, setIsLoading] = useState(null)
+  useEffect( () => {
+    setTimeout(()=> {setIsLoading(true)
+      // fetch('').then(res=> {
+      //   return res.json()
+      // })
+      // .then(data => {
+      //   setIsLoading(false)
+      // })
+    }, 1000)
+  }, [clientInfo])
+
+  const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+    { name: 'Group E', value: 278 },
+    { name: 'Group F', value: 189 },
+  ]
 
   return (
-    <div className="display">
-      <Avatar>
-        <PersonIcon />
-      </Avatar>
-      <h1> Heng Jiu Xiao</h1>
-      <h2> Total Assets: </h2>
+    
+    <div className='Display'>
+
+      <Paper variant='outlined' square>
+        <Avatar>
+          <PersonIcon />
+        </Avatar>
+        <h2>{clientInfo.title}</h2>
+        <h3> Sex: </h3>
+        <h3> Asset Allocation </h3>
+        <PieChart width={400} height={300}>
+          <Pie
+            dataKey="value"
+            startAngle={0}
+            endAngle={360}
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+        </PieChart>
+        <Button variant="text">More information</Button>
+      </Paper>
+      
+
     </div>
   );
 }
