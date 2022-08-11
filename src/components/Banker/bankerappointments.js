ca4fcd9397e1971ad886289b2d207f7737532ac6
hengjuice <hengjiuxiao@gmail.com>
2022-08-11 23:23:14 +0800

    Juice: Added backend call (commented out)

--- src/components/Banker/bankerappointments.js
+++ src/components/Banker/bankerappointments.js
@@ -2,7 +2,7 @@ import React from "react";
 import BankerCalender from "../Calendar/bankercalendar";
 import Display from '../Display/display'
 import { Grid } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BankerAppBar from "../Nav/bankernav";
 
 const BankerAppointment = () => {
@@ -12,6 +12,22 @@ const BankerAppointment = () => {
         { startDate: '2022-08-11T09:45', endDate: '2022-08-11T11:00', title: 'Loh Yi Ze'},
         { startDate: '2022-08-11T12:00', endDate: '2022-08-11T13:30', title: 'Heng Jiu Xiao'},
     ]
+    // TODO
+    // const [schedulerData, setSchedulerData] = useState([]);
+    // useEffect(()=>{
+    //     console.log('use effect ran');
+    //     // Backend Query insert here
+
+    //     fetch('http://localhost:4000/api/banker')
+    //         .then(res => {
+    //             return res.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //             setData(data)
+    //         })
+    // }, [])
+
     //fetch client data
 
     const [isLoading, setLoading] = useState(false);
