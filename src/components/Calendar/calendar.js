361ad47a87f86679f6115673328cba4e3e5dc437
hengjuice <hengjiuxiao@gmail.com>
2022-08-12 01:29:01 +0800

    JUICE: Changed save appointment to synchronous implementation

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -7,26 +7,52 @@ import { useState } from 'react';
 const CalenderGrid = (props) => {
 
     const [isLoading, setLoading] = useState(false); 
-    const saveAppointment= async (data) =>{
-        const response = await fetch('https://localhost:4000/api/appts', {
-        method: 'POST',
-        body: JSON.stringify({
-           apptId: "appointment" ,
-           endDate: data.endDate,
-           startDate: data.startDate,
-           notes: data.notes,
-           clientId: 1234,
-           bankerId: 5678,
-           title: data.title,
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+
+
+    // const saveAppointment= async (data) =>{
+    //     const response = await fetch('https://localhost:4000/api/appts', {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //        apptId: "appointment" ,
+    //        endDate: data.endDate,
+    //        startDate: data.startDate,
+    //        notes: data.notes,
+    //        clientId: 1234,
+    //        bankerId: 5678,
+    //        title: data.title,
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
         
-    })
-    const json = await response.json()
-    console.log(json)
+    // })
+    // const json = await response.json()
+    // console.log(json)
+    // }
+
+    const saveAppointment = (data) => {
+        fetch('http://localhost:4000/api/appts', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                apptId: "appointment" ,
+                endDate: data.endDate,
+                startDate: data.startDate,
+                notes: data.notes,
+                clientId: 1234,
+                bankerId: 5678,
+                title: data.title,
+            })
+        }).then(()=>{
+            console.log("Has been posted")
+        }).catch((e)=>{
+            console.log("Error posting")
+        })
     }
+
     // const saveAppointment =(data) =>{
     //     console.log(data)
     // }
