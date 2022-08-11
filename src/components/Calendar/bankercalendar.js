e584ada0c0bcac95a62c0fa988c0984dcb3434e0
malcolmpang19 <70058856+malcolmpang19@users.noreply.github.com>
2022-08-11 21:37:38 +0800

    bankercalendar

---
+++ src/components/Calendar/bankercalendar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
+import { Scheduler, WeekView, Appointments, AppointmentForm } from '@devexpress/dx-react-scheduler-material-ui';
+import Paper from '@mui/material/Paper';
+const BankerCalender = (props) => {
+    const appointments = props.appointments
+    const handleClick = props.handleClick
+    
+    const appointmentOptions = ({ data,children, style, ...restProps }) => {
+
+        return (
+        <Appointments.Appointment
+         {...restProps}
+         style={{
+           ...style,
+           backgroundColor: "#a02d37",
+           borderRadius: "8px",
+         }} 
+          onClick={()=>{
+            handleClick(data)
+           }}
+          >
+         {children}
+         </Appointments.Appointment>
+         );
+        }
+
+    return (
+        <div className="calendar">
+            <Paper>
+                <Scheduler
+                    data= {appointments}>
+
+                    <ViewState />
+                    <EditingState />
+                    <IntegratedEditing />
+                    <WeekView
+                        startDayHour={9}
+                        endDayHour={17}
+                    />
+                    <Appointments appointmentComponent={appointmentOptions}/>
+                    <AppointmentForm />
+                </Scheduler>
+            </Paper>
+            
+        </div>
+            
+
+    );
+}
+
+export default BankerCalender;
