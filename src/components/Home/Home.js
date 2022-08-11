c5907d0161a2f88451646b55f89c331a290beba4
malcolmpang19 <70058856+malcolmpang19@users.noreply.github.com>
2022-08-12 07:30:13 +0800

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ResponsiveAppBar from "../Nav/nav";
 
+
 const Home = () => {
     return (
         <div className="Home"> 
             <ResponsiveAppBar />
-            Homepage. Include what is needed for home page </div>
+        </div>
     );
 };
 
